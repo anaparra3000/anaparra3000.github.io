4497ebaab0fd16590510b04a6bd1c55eb8a7f160
aparr075 <anaparradev@gmail.com>
2021-09-28 02:09:49 -0400

    contact section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,7 +5,7 @@ export default function Contact() {
     <section id="contact" className="relative">
       <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap font-mono">
           <div className="relative flex flex-wrap py-6">
-            <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
+            <div className="px-6 mt-4 lg:mt-0">
               <a className="text-indigo-400 leading-relaxed" href="mailto:anaparradev@gmail.com">shoot me an email</a>
               <h2 className="title-font font-semibold text-black tracking-widest text-xs mt-4">
                 954-702-6434
