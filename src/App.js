a928acc1faeb8fb2c5e3f29c35af113326765ba7
anaparra <68021445+anaparra3000@users.noreply.github.com>
2022-12-10 22:44:31 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,16 @@ import Contact from "./components/Contact";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-H1QRYYPZ1Z"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-H1QRYYPZ1Z');
+</script>
+
 export default function App() {
   return (
     <main>
